931732d6e082e6d1275edc63b774f3f7d739f5b1
Sandeep Kumar <kumarsandeep110104@gmail.com>
2024-07-20 23:55:08 +0530

    Day_05 of 45 Days DSA Challenge

---
+++ 11. Container With Most Water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        temp = 0
+        area = 0
+        j = (len(height) - 1)
+        i = 0
+        if j == 1:
+            area = 1 * min(height)
+            return area
+        while i < len(height) and j > i:
+            if height[i] > height[j]:
+                temp = height[j] * (j-i)
+                if temp > area:
+                    area = temp
+                j -= 1
+            else:
+                temp = height[i] * (j-i)
+                if temp > area:
+                    area = temp
+                i += 1
+        return area
